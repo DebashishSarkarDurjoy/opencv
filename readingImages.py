86f329ec84c0d2a6c6206b0219febcd476499b31
dsdjoy4@gmail.com <dsdjoy4@gmail.com>
2021-12-25 22:21:25 +1100

    Reading videos

--- readingImages.py
+++ readingImages.py
@@ -1,12 +1,12 @@
 import cv2
 
-cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture("Resources/test_video.mp4")
 cap.set(3, 340)
 cap.set(4, 560)
 
 while True:
     success, img = cap.read()
-    cv2.imshow("Web Cam", img)
+    cv2.imshow("Reading Video", img)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
